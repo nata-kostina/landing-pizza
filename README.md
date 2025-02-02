ed0791c47841ea8a33f069b630f05dfb3139722a
Natallia <natakostina123@gmail.com>
2025-02-02 10:36:23 +0100

    implement landing page

---
+++ README.md
@@ -0,0 +1,48 @@
+# 🍕 Landing Page "Pizza Cheff"
+
+A fully responsive landing page designed to match the provided pizza-themed layout.
+
+## 🌐 Deploy
+You can view the deployed version [here]().
+
+## 🚀 Features
+
+- **Responsive Design:** Adaptive layout for the header and the first section of the landing page (optional for the entire site).
+- **Lightbox Integration:** Fullscreen image view for product images, regardless of their original size.
+- **Form Validation:** Standard validation with an additional rule to restrict dots (.) in the "Name" field.
+
+
+## 🛠️ Built With
+
+- **HTML & CSS:** Clean and semantic code without frameworks.
+- **JavaScript:** Vanilla JS for core functionality.
+- **jQuery:** Simplifies DOM manipulation.
+- **Lightbox Plugin:** For fullscreen image previews.
+
+## 📦 Getting Started
+
+To clone and run this project locally:
+
+```bash
+# Clone this repository
+git clone https://github.com/nata-kostina/landing-pizza.git
+
+# Navigate to the project folder
+cd landing-pizza
+
+# Launch the project using Live Server
+# (Ensure you have the Live Server extension installed in your code editor)
+```
+
+
+## ✨ Usage
+
+- **Image Lightbox:** Click on any product image to view it in fullscreen mode.
+- **Form Submission:** Fill out the form at the bottom of the page. Dots (.) are not allowed in the name field.
+
+## 📬 Contact
+
+- Email: [natakostina123@gmail.com](mailto:natakostina123@gmail.com)
+- Telegram: [@kostinata](https://t.me/kostinata)
+
+---
---
+++ index.js
@@ -0,0 +1,397 @@
+const _BREAKPOINTS = {
+    mobile: 768,
+    tablet: 1064,
+    desktopS: 1440
+};
+
+let breakpoint;
+let overlay;
+let popUp;
+
+document.addEventListener("DOMContentLoaded", function () {
+    init();
+});
+
+async function init() {
+    try {
+        $(".input-phone").inputmask("+7 (999) 999 99 99");
+
+        /* Fetch data */
+        const data = await fetch("./data.json", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+            }
+        })
+            .then((response) => response.json())
+            .catch(() => { throw new Error("Data is not found") })
+        const { menu, features, products } = data;
+
+        /* Handle resize */
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        /* Initialize components */
+        popUp = new PopUp();
+
+        overlay = document.querySelector(".overlay");
+
+        const Nav = new Navigation(menu);
+        Nav.init();
+
+        initFeatures(features);
+
+        initCatalogue(products);
+
+        initOrderForm();
+
+        lightbox.option({
+            "resizeDuration": 700,
+            "wrapAround": true,
+            "showImageNumberLabel": false,
+            "fitImagesInViewport": true,
+            "positionFromTop": 50,
+            alwaysShowNavOnTouchDevices: true,
+            disableScrolling: true,
+        });
+
+        initSmoothScroll();
+
+        /* Show page */
+        document.body.classList.remove("hidden");
+
+    } catch (error) {
+
+    }
+}
+
+class Navigation {
+    constructor(menu) {
+        this.menu = menu;
+        this.navDesk = document.querySelector(".nav-desktop");
+        this.navMobile = document.querySelector(".nav-mobile");
+        this.btnOpen = document.querySelector(".btn-hamburger");
+        this.btnClose = this.navMobile.querySelector(".btn-close");
+        window.addEventListener("breakpointChange", this.handleBpChange.bind(this));
+        this.btnOpen.addEventListener("click", () => {
+            overlay?.classList.remove("hidden");
+            this.navMobile.classList.add("slided");
+            fixWindow(true);
+        })
+        this.btnClose.addEventListener("click", () => {
+            overlay?.classList.add("hidden");
+            this.navMobile.classList.remove("slided");
+            fixWindow(false);
+        })
+    }
+
+    init() {
+        this.menu.forEach((item) => {
+            const a = document.createElement("a");
+            a.classList = "nav__item t-small";
+            a.href = item.link;
+            a.innerText = item.title;
+            this.navMobile.appendChild(a);
+            this.navDesk.appendChild(a.cloneNode(true));
+        });
+        if (breakpoint === "mobile" || breakpoint === "tablet") {
+            this.showMobileNav();
+        } else {
+            this.showDesktopNav();
+        }
+    }
+
+    showMobileNav() {
+        this.navDesk.classList.add("hidden");
+        this.navMobile.classList.remove("hidden");
+    }
+
+    showDesktopNav() {
+        this.navMobile.classList.add("hidden");
+        this.navDesk.classList.remove("hidden");
+    }
+
+    handleBpChange() {
+        if (breakpoint === "mobile" || breakpoint === "tablet") {
+            this.showMobileNav();
+        } else {
+            this.showDesktopNav();
+        }
+    }
+}
+
+function getBreakpoint() {
+    const width = window.innerWidth;
+
+    if (width <= _BREAKPOINTS.mobile) {
+        return "mobile";
+    } else if (width <= _BREAKPOINTS.tablet) {
+        return "tablet";
+    } else if (width <= _BREAKPOINTS.desktopS) {
+        return "desktop-s";
+    } else {
+        return "desktop";
+    }
+}
+
+function handleResize() {
+    const newBreakpoint = getBreakpoint();
+
+    if (newBreakpoint !== breakpoint) {
+        breakpoint = newBreakpoint;
+        const event = new CustomEvent("breakpointChange");
+        window.dispatchEvent(event);
+    }
+
+}
+
+function initFeatures(features) {
+    const featuresBox = document.querySelector(".features");
+    features.forEach(({ title, text, img }) => {
+        const item = document.createElement("div");
+        item.classList = "feature";
+        const image = document.createElement("img")
+        image.classList = "feature__img";
+        image.src = img;
+        image.alt = title;
+        const ttl = document.createElement("h3");
+        ttl.classList = "feature__title t3";
+        ttl.innerHTML = title;
+        const description = document.createElement("span");
+        description.classList = "feature__description t-body";
+        description.innerHTML = text;
+        item.append(image, ttl, description);
+        featuresBox.appendChild(item);
+    })
+}
+
+let fixWindowTimeout = 0;
+
+function fixWindow(type) {
+    const htmlElement = document.documentElement;
+    if (type) {
+        fixWindowTimeout = setTimeout(() => {
+            htmlElement.style.overflow = "hidden";
+            document.body.style.overflow = "hidden";
+            document.body.style.touchAction = "none";
+            document.body.style.pointerEvents = "none";
+        }, 20);
+    } else {
+        htmlElement.style.height = "";
+        document.body.style.height = "";
+        htmlElement.style.overflow = "";
+        document.body.style.overflow = "";
+        document.body.style.touchAction = "";
+        document.body.style.pointerEvents = "";
+        clearTimeout(fixWindowTimeout);
+    }
+}
+
+function initCatalogue(products) {
+    const catalogue = document.querySelector(".catalogue");
+    products.forEach(({ img, title, ingredients }) => {
+        const item = document.createElement("div");
+        item.classList = "product";
+        const lb = document.createElement("a");
+        lb.href = img;
+        lb.dataset.lightbox = "product-gallery";
+        lb.dataset.title = title;
+        lb.classList = "product__img";
+        const image = document.createElement("img");
+        image.classList = "img";
+        image.src = img;
+        image.alt = title;
+        lb.append(image);
+        const ttl = document.createElement("h4");
+        ttl.className = "product__title t4";
+        ttl.innerHTML = title;
+        const description = document.createElement("span");
+        description.classList = "product__ingredient t-small-xs";
+        description.innerHTML = ingredients;
+        const btn = document.createElement("button")
+        btn.classList = "btn btn-primary btn-add-to-cart t-btn";
+        btn.type = "button";
+        btn.innerText = "В корзину";
+        item.append(lb, ttl, description, btn);
+        catalogue.appendChild(item);
+    });
+}
+
+function initOrderForm() {
+    const form = document.querySelector(".form-order");
+    const nameInput = form.querySelector(".input-name");
+    const nameInputError = form.querySelector(".input-error-name");
+    const addressInput = form.querySelector(".input-address");
+    const addressInputError = form.querySelector(".input-error-address");
+    const phoneInput = form.querySelector(".input-phone");
+    const phoneInputError = form.querySelector(".input-error-phone");
+
+    const formInputError = form.querySelector(".input-error-form");
+    const btnSubmit = form.querySelector(".btn-submit");
+
+    nameInput.addEventListener("input", function () {
+        nameInput.value = nameInput.value.replace(/[.\d]/g, "");
+        nameInputError.classList.add("hidden");
+    });
+
+    nameInput.addEventListener("blur", () => {
+        const result = validateName(nameInput.value);
+        if (!result.isValid) {
+            nameInputError.innerText = result.message ?? "";
+            nameInputError.classList.remove("hidden");
+        }
+    })
+
+    addressInput.addEventListener("input", function () {
+        addressInputError.classList.add("hidden");
+    });
+
+    addressInput.addEventListener("blur", () => {
+        const result = validateAddress(addressInput.value);
+        if (!result.isValid) {
+            addressInputError.innerText = result.message ?? "";
+            addressInputError.classList.remove("hidden");
+        }
+    })
+
+    $(".input-phone").on("input", function () {
+        phoneInputError.classList.add("hidden");
+    });
+
+    phoneInput.addEventListener("blur", () => {
+        const result = validatePhone();
+        if (!result.isValid) {
+            phoneInputError.innerText = result.message ?? "";
+            phoneInputError.classList.remove("hidden");
+        }
+    })
+
+    form.addEventListener("input", () => {
+        formInputError.classList.add("hidden");
+    });
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        btnSubmit.disabled = true;
+        const formData = new FormData(form);
+        const result = validateForm(formData);
+
+        if (result.isValid) {
+            try {
+                const pendingRequest = () => new Promise((res) => {
+                    setTimeout(() => res({ status: "success" }), 1000);
+                }, (rej) => {
+                    setTimeout(() => res({ status: "error" }), 1000);
+                });
+
+                const response = await pendingRequest();
+
+
+                if (response.status === "error") {
+                    popUp.open("Ошибка. Заказ не был оформлен.");
+                } else {
+                    popUp.open("Спасибо за заказ");
+                }
+
+            } catch (error) {
+                formInputError.innerText = "Ошибка сети. Заказ не был оформлен.";
+                formInputError.classList.remove("hidden");
+            } finally {
+                form.reset();
+                btnSubmit.disabled = false;
+            }
+        }
+    })
+}
+
+function validateForm(formData) {
+    const data = Object.fromEntries(formData);
+    const validatedName = validateName(data.name);
+    const validatedAddress = validateAddress(data.address);
+    const validatedPhone = validatePhone(data.phone);
+    return {
+        isValid: [validatedName.isValid, validatedAddress.isValid, validatedPhone.isValid].some((item) => !!item),
+        details: {
+            name: validatedName,
+            address: validatedAddress,
+            phone: validatedPhone
+        }
+    };
+}
+
+function isInputEmpty(value) {
+    return value.replace(/\s/g, "") === "";
+}
+
+function validateName(value) {
+    return !isInputEmpty(value) ? { isValid: true } : { isValid: false, message: "Введите имя" };
+}
+
+function validateAddress(value) {
+    return !isInputEmpty(value) ? { isValid: true } : { isValid: false, message: "Введите адрес" };
+}
+
+function validatePhone() {
+    const rawPhone = $(".input-phone").inputmask("unmaskedvalue");
+    return !isInputEmpty(rawPhone) ? { isValid: true } : { isValid: false, message: "Введите телефон" };
+}
+
+
+class PopUp {
+    constructor() {
+        this.popup = document.querySelector(".pop-up");
+        this.messageBox = this.popup.querySelector(".pop-up__message");
+        this.btnClose = this.popup.querySelector(".btn-close");
+        this.btnClose.addEventListener("click", () => this.close());
+        this.boundedClose = this.close.bind(this);
+    }
+
+    open(message) {
+        overlay.style.pointerEvents = "all";
+        overlay.addEventListener("click", this.boundedClose);
+        overlay?.classList.remove("hidden");
+        this.popup.classList.add("active");
+        setTimeout(() => {
+            this.messageBox.innerHTML = message;
+            this.popup.classList.add("animated");
+        }, 50);
+    }
+
+    close() {
+        overlay.removeEventListener("click", this.boundedClose);
+        overlay.style.pointerEvents = "none";
+        overlay?.classList.add("hidden");
+        this.popup.classList.remove("animated");
+        setTimeout(() => {
+            this.messageBox.innerHTML = "";
+            this.popup.classList.remove("active");
+        }, 250);
+    }
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+    let target = "";
+    if (e.currentTarget.href) {
+        target = e.currentTarget.href.substring(2);
+    } else {
+        target = e.currentTarget.dataset.anchor ?? "";
+    }
+    if (!target) { return; }
+    const scrollTarget = document.querySelector(target);
+
+    if (scrollTarget === null) return;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    window.scrollBy({
+        top: elementPosition,
+        behavior: "smooth",
+    });
+};
+
+function initSmoothScroll() {
+    const elementsWithAnchor = document.querySelectorAll("[data-anchor]");
+    elementsWithAnchor.forEach((el) => {
+        el.addEventListener("click", (e) => smoothScroll(e))
+    });
+}
